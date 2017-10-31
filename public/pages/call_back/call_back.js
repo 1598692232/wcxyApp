610cab600c7f1c6fa1e66f37f20a08e9fee0929e
1037359589 <1037359589@qq.com>
2017-10-31 15:43:08 +0800

    评论回复代码优化

--- public/pages/call_back/call_back.js
+++ public/pages/call_back/call_back.js
@@ -19,91 +19,66 @@ Page({
         tsy: '',
         delTouching: false,
         showDel: false,
+        sendCallback: false,
+        delCallback: false
     },
 
    onLoad(options) {
         let self = this
         wx.showLoading()
-        wx.getSystemInfo({
-            success: function (res) {
-                self.setData({
-                    scrollHeight: res.windowHeight - 54,
-                    commentId: parseInt(options.commentId),
-                    docId: parseInt(options.docId),
-                    avatar: options.avatar == '' ? tx : options.avatar 
-                });
-
-                let store = wx.getStorageSync('app')
-                let reqData = Object.assign({}, store, {
-                    doc_id: self.data.docId,
-                    project_id: wx.getStorageSync('project_id') || options.projectId
-                })
-                wx.request({
-                    url: store.host + '/wxapi/comment',
-                    data: reqData,
-                    header: {
-                        'content-type': 'application/json' // 默认值
-                    },
-                    method: 'get',
-                    success: function(res) {
-                        if (res.data.status == 1) {
-                            let appStore = wx.getStorageSync('app')
-                            let currentComment = res.data.data.list.filter(item => {
-                                return parseInt(item.id) == self.data.commentId
-                            })
-
-                            currentComment[0].comment_time = Util.timeToMinAndSec(currentComment[0].media_time)
-
-                            currentComment[0].replies.map(item => {
-                                item.translateX = '',
-                                item.delTranstion = ''
-                                item.avatar = item.avatar == '' ? self.data.tx : item.avatar
-                                if(appStore.login_id == item.user_id) {
-                                    item.delColor = '#f00'
-                                } else {
-                                    item.delColor = '#ddd'
-                                }
-                            })
-                            self.setData({
-                                callList: currentComment[0].replies,
-                                currentComment: currentComment[0]
-                            })
-                            wx.setNavigationBarTitle({title: `${currentComment[0].replies.length}条回复`})
-                            wx.hideLoading()
-                        } else {
-                            wx.showModal({
-                              title: '提示',
-                              content: '获取回复信息失败！',
-                            })
-                        }
-                    },
-                    fail(res) {
-                        wx.showModal({
-                            title:'提示',
-                            content: JSON.stringify(res),
-                        })
-                    }
-                })
-
-            }
-        });
+        self.setData({
+            commentId: parseInt(options.commentId),
+            docId: parseInt(options.docId),
+            avatar: options.avatar == '' ? tx : options.avatar 
+        })
     },
 
     onShow() {
-        // 评论输入框动画注册
-        let animation1 = wx.createAnimation({
-            duration: 0,
-            timingFunction: 'ease',
-        })
+        let self = this
+        Util.getSystemInfo().then(res => {
+            self.setData({
+                scrollHeight: res.windowHeight - 54,
+            })
+        }).then(() => {
 
-        this.animation1 = animation1
+            let store = wx.getStorageSync('app')
+            let reqData = Object.assign({}, store, {
+                doc_id: self.data.docId,
+                project_id: wx.getStorageSync('project_id') || options.projectId
+            })
+    
+            Util.ajax('comment', 'get', reqData).then(json => {
+                let appStore = wx.getStorageSync('app')
+                let currentComment = json.list.filter(item => {
+                    return parseInt(item.id) == self.data.commentId
+                })
+    
+                currentComment[0].comment_time = Util.timeToMinAndSec(currentComment[0].media_time)
+    
+                currentComment[0].replies.map(item => {
+                    item.translateX = '',
+                    item.delTranstion = ''
+                    item.avatar = item.avatar == '' ? self.data.tx : item.avatar
+                    if(appStore.login_id == item.user_id) {
+                        item.delColor = '#f00'
+                    } else {
+                        item.delColor = '#ddd'
+                    }
+                })
+                self.setData({
+                    callList: currentComment[0].replies,
+                    currentComment: currentComment[0]
+                })
+                wx.setNavigationBarTitle({title: `${currentComment[0].replies.length}条回复`})
+            }, res => {
+                wx.showModal({
+                    title: '提示',
+                    content: '获取回复信息失败！',
+                    showCancel: false
+                })
+            })
 
-        let animation2 = wx.createAnimation({
-            duration: 100,
-            timingFunction: 'ease',
-        })
-
-        this.animation2 = animation2
+        }) 
     },
 
      // 发送评论
@@ -119,78 +94,52 @@ Page({
             project_id: wx.getStorageSync('project_id'),
             top_id: self.data.commentId
         })
-        wx.request({
-            url: store.host + '/wxapi/comment',
-            data: reqData,
-            header: {
-                'content-type': 'application/json' // 默认值
-            },
-            method: 'post',
-            success: function(res) {
-                wx.hideLoading()
-                if (res.data.status == 1) {
-                    let list = self.data.callList
-                    wx.showToast({
-                        title: '回复成功！！'
-                    })
-                    let newCall= {
-                        content: e.detail.value.commentText,
-                        comment_time: Util.timeToMinAndSec(self.data.videoTime),
-                        doc_id: self.data.docId,
-                        project_id: wx.getStorageSync('project_id'),
-                        id: res.data.data.id,
-                        realname:wx.getStorageSync('user_info').realname,
-                        avatar: wx.getStorageSync('user_info').avatar == '' ? self.data.tx : wx.getStorageSync('user_info').avatar,
-                        translateX: '',
-                        delTranstion: '',
-                        delColor: '#f00',
-                        user_id: wx.getStorageSync('app').login_id
-                    }
 
-                    list.unshift(newCall)
+        if (self.data.sendCallback) return
+        self.data.sendCallback = true
 
-                    self.setData({
-                        callList: list,
-                        commentText: ''
-                    })
-
-                } else {
-                    wx.showModal({
-                      title: '提示',
-                      content: '发表评论失败！',
-                      showCancel: false,
-                    })
-
-                }
+        Util.ajax('comment', 'post', reqData).then(json => {
+            let list = self.data.callList
+            wx.showToast({
+                title: '回复成功！！'
+            })
+            let newCall= {
+                content: e.detail.value.commentText,
+                comment_time: Util.timeToMinAndSec(self.data.videoTime),
+                doc_id: self.data.docId,
+                project_id: wx.getStorageSync('project_id'),
+                id: json.id,
+                realname:wx.getStorageSync('user_info').realname,
+                avatar: wx.getStorageSync('user_info').avatar == '' ? self.data.tx : wx.getStorageSync('user_info').avatar,
+                translateX: '',
+                delTranstion: '',
+                delColor: '#f00',
+                user_id: wx.getStorageSync('app').login_id
             }
+
+            list.unshift(newCall)
+
+            self.setData({
+                callList: list,
+                commentText: ''
+            })
+        }, res => {
+            wx.showModal({
+                title: '提示',
+                content: '发表评论失败！',
+                showCancel: false,
+            })
+        }).then(() => {
+            self.data.sendCallback = false
         })
      },
 
-
-    // loadMore() {
-    //     let data = this.data.callList
-    //     this.setData({
-    //       page: ++this.data.page
-    //     })
-    //     for(let i = 10 * (this.data.page - 1) + 1; i <= this.data.page * 10; i ++ ){
-    //       data.push(i)
-    //     }
-    //     this.setData({
-    //         callList: data,
-    //     })
-    // },
-
-    // 删除评论 start
+    // 删除回复 start
     delTouchStart(e) {
-        // let realname1 = this.data.callList[e.currentTarget.dataset.index].realname
-        // let realname2 = wx.getStorageSync('user_info').realname
-
         this.data.callList.map(item => {
             item.translateX = '',
             item.delTranstion = ''
         })
-
-        // if (realname1 != realname2) return
 
         this.setData({
             tsx: e.touches[0].clientX,
@@ -212,13 +161,6 @@ Page({
                moveX = -100
             }
 
-            // if (moveX < -60) {
-            //     moveX = -(Math.pow(Math.abs(moveX + 60), 0.8) + 60)
-            // }
-            // if (moveX > 0) {
-            //     moveX = Math.pow(Math.abs(moveX + 60), 0.5)
-            // }
-
             this.data.callList[e.currentTarget.dataset.index].translateX = moveX
             this.setData({
                 callList: this.data.callList,
@@ -230,14 +172,6 @@ Page({
             if (moveX > 100) {
                 moveX = 0
             } 
-
-            // if (moveX > 60) {
-            //     moveX = Math.pow(Math.abs(moveX - 60), 0.5)
-            // } else if (moveX < 0) {
-            //     moveX = -(Math.pow(Math.abs(moveX), 0.8) + 60)
-            // } else {
-            //     moveX = moveX - 60
-            // }
 
             this.data.callList[e.currentTarget.dataset.index].translateX = moveX
             this.setData({
@@ -304,32 +238,27 @@ Page({
         wx.showLoading({
             title: '正在删除...'
         })
-        wx.request({
-            url: store.host + '/wxapi/comment',
-            data: reqData,
-            method: 'delete',
-            header: {
-                'content-type': 'application/json' // 默认
-            },
-            success(res) {
-                if (res.data.status == 1) {
-                    self.data.callList.splice(e.currentTarget.dataset.index, 1)
-                    self.setData({
-                        callList: self.data.callList
-                    })
-                    wx.showToast({
-                        title: '删除成功！'
-                    })
-                } else {
-                    wx.showModal({
-                      title: '提示',
-                      content: '删除评论失败！',
-                      showCancel: false,
-                    })
-                }
-            }
+        
+        if(self.data.sendCallback) return
+        self.data.sendCallback = true
+
+        Util.ajax('comment', 'delete', reqData).then(json => {
+            self.data.callList.splice(e.currentTarget.dataset.index, 1)
+            self.setData({
+                callList: self.data.callList
+            })
+            wx.showToast({
+                title: '删除成功！'
+            })
+        }, res => {
+            wx.showModal({
+                title: '提示',
+                content: '删除评论失败！',
+                showCancel: false,
+            })
+        }).then(() => {
+            self.data.sendCallback = false
         })
     }
-    // 删除评论 end
-
+    // 删除回复 end
 })
