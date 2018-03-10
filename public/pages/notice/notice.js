2eb0b9b3c243d059b3a0e80b53d34567cf35b472
suworks <liuru621@163.com>
2018-03-10 19:38:47 +0800

    0309提交

--- public/pages/notice/notice.js
+++ public/pages/notice/notice.js
@@ -20,24 +20,10 @@ Page({
                 listInfoWidth: res.windowWidth - 110,
                 liWidth: res.windowWidth - 110
             })
-
-            let infoData = wx.getStorageSync('info_data')
-            if (infoData != '') {
-                setTimeout(() => {
-                    let url = '/pages/info/info?url=' + infoData.url + '&name='
-                    + infoData.name + '&id=' + infoData.doc_id
-                    + '&username=' + infoData.username + '&createTime=' + infoData.createTime
-                    + '&coverImg=' + infoData.coverImg + '&project_id=' + infoData.project_id
-                    wx.navigateTo({
-                        url: url
-                    })
-                }, 1000)
-            }
         })
     },
     onShow() {
         let store = wx.getStorageSync('app')
-
         if (store.token == '') {
             wx.navigateTo({
                 url: '/pages/signin/signin'
@@ -50,17 +36,39 @@ Page({
     },
     //初始化列表页
     initList() {
-        let store = wx.getStorageSync('app')
         let self = this
 
-        Util.ajax('project', 'get', store).then(data => {
-            let pros = []
-            data.list.forEach(item => {
-                pros.push(item)
+        //获取当前时间戳  
+        var timestamp = Date.parse(new Date());  
+        timestamp = timestamp / 1000;  
+        console.log("当前时间戳为：" + timestamp);
+        // ajax处理登录，成功后存储本地信息
+        // 本地存储用户信息
+        let store = wx.getStorageSync('app')
+        let reqData = Object.assign({}, {token: store.token},{login_id:store.login_id})
+        reqData.new_time = 1420572841
+        wx.showLoading()
+        Util.ajax('notice', 'get', reqData).then(data => {
+            data.list.map(item => {
+                console.log(wx.getStorageSync('last_time'),'wx.getStorageSync')
+                if(item.project_id === wx.getStorageSync('last_time')[0].id){
+                    item.time = wx.getStorageSync('last_time')[0].time     
+                } else {
+                    item.time = 1483200000
+                }
+                console.log(item.time,'item.time')
+                item.notice_content.map(v=>{
+                    if(v.created_at > item.time){
+                        // console.log(v,'vvvvvv')
+                    }
+                })
+                item.count = 0
             })
             self.setData({
-                noticeList: pros
+                noticeList: data.list,
+                // noticeListType: translateNotificationType(data.notice_content[0].type)
             })
+            console.log(self.data.noticeList,'-------list')
             wx.hideLoading()
         }, res => {
             wx.showModal({
@@ -69,34 +77,12 @@ Page({
                 showCancel: false,
                 success: function(res) {
                     if (res.confirm) {
-                        wx.navigateTo({url: '/pages/signin/signin?login_out=1'})
+                        wx.navigateTo({url: '/pages/signin/signin?login_out=2'})
                     }
                 }
             })
         })
-    },
-
-    consoleLoginError(errText) {
-        this.setData({
-            hiddenErr: false,
-            errMsg: errText
-        })
-        this.animation.opacity(1).step()
-        this.setData({
-            animationData: this.animation.export()
-        })
-
-        setTimeout(() => {
-            this.animation.opacity(0).step({duration: 1000})
-            this.setData({
-                animationData: this.animation.export(),
-            })
-            setTimeout(() => {
-                this.setData({
-                    hiddenErr: true,
-                })
-            }, 1000)
-        }, 2000)
+        
     },
     //跳转至系统消息
     toNoticeSystemInfo(e) {
@@ -106,8 +92,40 @@ Page({
     },
     // 跳转至消息详情列表页
     toNoticeInfo(e) {
-        wx.navigateTo({
-          url: '/pages/notice_list/notice_list'
+        var timestamp = Date.parse(new Date());
+        timestamp = timestamp / 1000;
+        let newStorage = Object.assign({},{id:e.currentTarget.dataset.id})
+        newStorage.time = timestamp
+        var arr = []
+        arr = arr.concat(newStorage)
+
+        wx.setStorage({
+            key: 'last_time',
+            data: arr
         })
-    }
+
+        let self = this
+        let url = '/pages/notice_list/notice_list?project_id=' + e.currentTarget.dataset.id 
+        + '&created_at=' + e.currentTarget.dataset.time
+        console.log(url,'url6666')
+        wx.navigateTo({
+            url: url,
+        })
+    },
+    // setAllNotice() {
+    //     wx.getStorageSync('last_time')
+    //     let last_time = []
+    //     this.data.noticeList.forEach(v => {
+    //         if( v.id === wx.getStorageSync('last_time').id) {
+    //             v.time = wx.getStorageSync('last_time').time
+    //         } else {
+    //             v.time = timestamp
+    //         }
+    //         last_time.push(v)
+    //     })
+    //     wx.setStorage({
+    //         key: 'last_time',
+    //         data: last_time
+    //     })
+    // },
 })
