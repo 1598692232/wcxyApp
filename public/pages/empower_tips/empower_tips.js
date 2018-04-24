e40dc31a972d6972308d71581fed65ed42a3cf5d
suworks <liuru621@163.com>
2018-04-24 15:08:54 +0800

    提交代码2---小程序授权，获取用户头像的修改

--- public/pages/empower_tips/empower_tips.js
+++ public/pages/empower_tips/empower_tips.js
@@ -8,7 +8,9 @@ Page({
         realName: '',
         avatar: '',
         content: '',
-        backIndex: false
+        backIndex: false,
+        needEmpower: false,
+        tips: ''
     },
 	onLoad(options) {
         console.log(options,'options')
@@ -22,13 +24,20 @@ Page({
                 })
             }
         })
+        if(options.tips){
+            self.setData({
+                needEmpower: true
+            })
+        }
         if(options.tips==1) {
             self.setData({
-                content: '你拒绝了授权，账号需要实名认证。请退出并删除小程序重新进入，再次点击授权。'
+                content: '你拒绝了授权，账号需要实名认证。请再次点击授权。',
+                tips: 1
             })
         } else if (options.tips==2) {
             self.setData({
-                content: '你拒绝了授权，项目分享者无法获取您的信息。请退出并删除小程序重新进入，再次点击授权。'
+                content: '你拒绝了授权，项目分享者无法获取您的信息。请再次点击授权。',
+                tips: 2
             })
         } else if (options.sign==1) {
             self.setData({
@@ -50,6 +59,44 @@ Page({
         })
     },
     bindgetuserinfo() {
-        
+        let self = this
+        wx.getUserInfo({
+            success: function(res) {
+                var userInfo = res.userInfo
+                var nickName = userInfo.nickName
+                var avatarUrl = userInfo.avatarUrl
+
+                let store = wx.getStorageSync('app')
+                let reqData2 = Object.assign({},{login_id:store.login_id},{token:store.token},{nick_name: nickName},{avatar: avatarUrl})
+                Util.ajax('user/info', 'post', reqData2).then((data) => {
+                    wx.setStorage({
+                        key: 'user_info',
+                        data: data
+                    })
+                }, res => {
+                    wx.showModal({
+                        title: '提示',
+                        content: res.data.msg,
+                        showCancel: false
+                    })
+                })
+                if( self.data.tips==1 ){
+                    wx.reLaunch({
+                        url: '/pages/empower_signin/empower_signin'
+                    })
+                }else if( self.data.tips==2 ){
+                    wx.reLaunch({
+                        url: '/pages/share_list_view/share_list_view'
+                    })
+                }
+            },
+            fail() {
+                wx.showModal({
+                    title: '提示',
+                    content: '授权失败',
+                    showCancel: false
+                })
+            }
+        })
     }
 })
